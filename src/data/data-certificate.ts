6ee3cbcaeef80d22e2d10885a064a1331dc7154e
Vinicius Holtman <viniholtman28@gmail.com>
2023-01-10 11:35:15 -0300

    Created file with data to certificates

---
+++ src/data/data-certificate.ts
@@ -0,0 +1,245 @@
+import certificate1 from "../assets/certificates/Como_Gerenciar_o_Estado_das_Aplicacoes_com_Redux.png"
+import certificate2 from "../assets/certificates/Trabalhando_com_Componentes_em_Angular.png"
+import certificate3 from "../assets/certificates/Configurando_Ambient_de_Desenvolvimento_Java_no_Linux.png"
+import certificate4 from "../assets/certificates/Criando_uma_API_Rest_Documentada_com_SpringWeb_e_Swagger.png"
+import certificate5 from "../assets/certificates/Desenvolvendo_Rest_APIs_com_Python_e_Flask.png"
+import certificate6 from "../assets/certificates/Desenvolvimento_Backend_com_NodeJs.png"
+import certificate7 from "../assets/certificates/Desenvolvimento_Basico_em_Java.png"
+import certificate8 from "../assets/certificates/Desenvolvimento_Web_com_Python_e_Django.png"
+import certificate9 from "../assets/certificates/Fundamentos_em_Gestao_de_Projetos.jpg"
+import certificate10 from "../assets/certificates/Introducao_a_programacao_com_Python.png"
+import certificate11 from "../assets/certificates/Introducao_ao_Git_e_ao_Github.png"
+import certificate12 from "../assets/certificates/Introducao_ao_NodeJs_com_Express.png"
+import certificate13 from "../assets/certificates/Introducao_ao_PHP.png"
+import certificate14 from "../assets/certificates/Introducao_ao_Typescript_Explorando_Classes_Tipos_e_Interfaces.png"
+import certificate15 from "../assets/certificates/Introducao_aos_Conceitos_de_API_e_Clean_Architecture.png"
+import certificate16 from "../assets/certificates/Javascript_e_Typescript.jpg"
+import certificate17 from "../assets/certificates/NestJs_com_TypeORM_Mongoose_Prisma_Swagger.jpg"
+import certificate18 from "../assets/certificates/NodeJs_Ignite_Rocketseat.png"
+import certificate19 from "../assets/certificates/Praticando_Orientação_a_Objetos_com_Java.png"
+import certificate20 from "../assets/certificates/Projetos_ageis_com_SCRUM.png"
+import certificate21 from "../assets/certificates/ReactJs_Ignite_Rocketseat.png"
+import certificate22 from "../assets/certificates/Trabalhando_com_Componentes_em_React.png"
+
+export const certificates = [
+  {
+    certificateName: "Como Gerenciar o Estado das Aplicações com Redux",
+    company: "DIO",
+    issueDate: "Jan 2023",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "4E831873",
+    credentialUrl: "www.dio.me/certificate/4E831873",
+    certificateImage: certificate1
+  },
+  {
+    certificateName: "Trabalhando com Componetes em Angular",
+    company: "DIO",
+    issueDate: "Out 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "6433F554",
+    credentialUrl: "www.dio.me/certificate/6433F554",
+    certificateImage: certificate2
+  },
+  {
+    certificateName: "Configurando Ambiente de Desenvolvimento Java no Linux",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "2358488D",
+    credentialUrl: "www.dio.me/certificate/2358488D",
+    certificateImage: certificate3
+  },
+  {
+    certificateName: "Criando uma API Rest Documentada com Spring Web e Swagger",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "DCC024E6",
+    credentialUrl: "www.dio.me/certificate/DCC024E6",
+    certificateImage: certificate4
+  },
+  {
+    certificateName: "Desenvolvendo Rest APIs Com Python e Flask",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "A691CA6F",
+    credentialUrl: "www.dio.me/certificate/A691CA6F",
+    certificateImage: certificate5
+  },
+  {
+    certificateName: "Desenvolvimento back-end com Node.js",
+    company: "DIO",
+    issueDate: "Out 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "FDE350BD",
+    credentialUrl: "www.dio.me/certificate/FDE350BD",
+    certificateImage: certificate6
+  },
+  {
+    certificateName: "Desenvolvimento básico em Java",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "2B6E7676",
+    credentialUrl: "www.dio.me/certificate/2B6E7676",
+    certificateImage: certificate7
+  },
+  {
+    certificateName: "Desenvolvimento Web com Python e Django",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "B074DB53",
+    credentialUrl: "www.dio.me/certificate/B074DB53",
+    certificateImage: certificate8
+  },
+  {
+    certificateName: "Fundamentos em Gestão de Projetos",
+    company: "DIO",
+    issueDate: "Jun 2020",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "UC-a174d76e-cd15-4474-ac08-5cfa12f0f2df",
+    credentialUrl: "www.udemy.com/certificate/UC-a174d76e-cd15-4474-ac08-5cfa12f0f2df/",
+    certificateImage: certificate9
+  },
+  {
+    certificateName: "Introdução à programação com Python",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "8BA71B28",
+    credentialUrl: "www.dio.me/certificate/8BA71B28",
+    certificateImage: certificate10
+  },
+  {
+    certificateName: "Introdução ao Git e ao GitHub",
+    company: "DIO",
+    issueDate: "Nov 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "B550B7BB",
+    credentialUrl: "www.dio.me/certificate/B550B7BB",
+    certificateImage: certificate11
+  },
+  {
+    certificateName: "Introdução ao Node.js com Express",
+    company: "DIO",
+    issueDate: "Out 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "19014650",
+    credentialUrl: "www.dio.me/certificate/19014650",
+    certificateImage: certificate12
+  },
+  {
+    certificateName: "Introdução ao PHP",
+    company: "DIO",
+    issueDate: "Out 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "227C04F3",
+    credentialUrl: "www.dio.me/certificate/227C04F3",
+    certificateImage: certificate13
+  },
+  {
+    certificateName: "Introdução ao Typescript: Explorando Classes, Tipos e Interface",
+    company: "DIO",
+    issueDate: "Out 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "8B2502C7",
+    credentialUrl: "www.dio.me/certificate/8B2502C7",
+    certificateImage: certificate14
+  },
+  {
+    certificateName: "Introdução aos Conceitos de API e Clean Architecture",
+    company: "DIO",
+    issueDate: "Out 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "BADAA22F",
+    credentialUrl: "www.dio.me/certificate/BADAA22F",
+    certificateImage: certificate15
+  },
+  {
+    certificateName: "JavaScript e Typescript",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "UC-69147753-bc78-4004-8f73-49210d64ecce",
+    credentialUrl: "www.udemy.com/certificate/UC-69147753-bc78-4004-8f73-49210d64ecce/",
+    certificateImage: certificate16
+  },
+  {
+    certificateName: "NestJs com TypeORM, Mongoose, Prisma e Swagger",
+    company: "DIO",
+    issueDate: "Nov 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "UC-3e822e20-f5f3-4db5-a364-edc5750491cc",
+    credentialUrl: "www.udemy.com/certificate/UC-3e822e20-f5f3-4db5-a364-edc5750491cc/",
+    certificateImage: certificate17
+  },
+  {
+    certificateName: "NodeJs Ignite Rocketseat",
+    company: "DIO",
+    issueDate: "Nov 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "5a78c6d6-4a6a-4664-a1d2-0af778c6be95",
+    credentialUrl: "app.rocketseat.com.br/certificates/5a78c6d6-4a6a-4664-a1d2-0af778c6be95",
+    certificateImage: certificate18
+  },
+  {
+    certificateName: "Praticando Orientação a Objetos com Java",
+    company: "DIO",
+    issueDate: "Dez 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "D0BED76C",
+    credentialUrl: "www.dio.me/certificate/D0BED76C",
+    certificateImage: certificate19
+  },
+  {
+    certificateName: "Projetos ágeis com SCRUM",
+    company: "DIO",
+    issueDate: "Nov 2021",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "CE4AF8D8",
+    credentialUrl: "www.dio.me/certificate/CE4AF8D8",
+    certificateImage: certificate20
+  },
+  {
+    certificateName: "ReactJs Ignite Rocketseat",
+    company: "DIO",
+    issueDate: "Nov 2022",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "47f82426-13a4-46db-bec3-772c3c043e78",
+    credentialUrl: "app.rocketseat.com.br/certificates/47f82426-13a4-46db-bec3-772c3c043e78",
+    certificateImage: certificate21
+  },
+  {
+    certificateName: "Trabalhando com Componentes em React",
+    company: "DIO",
+    issueDate: "Jan 2023",
+    isExpired: false,
+    expiredDate: null,
+    credentialCod: "93A36612",
+    credentialUrl: "www.dio.me/certificate/93A36612",
+    certificateImage: certificate22
+  },
+]
