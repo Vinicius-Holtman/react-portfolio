76beb350d6751cbde98e8f5cc74263bc8bb38f7b
Vinicius Holtman <viniholtman28@gmail.com>
2023-12-11 02:26:55 -0300

    Criado serviço para enviado do email

--- src/pages/Home/ContactSection/index.tsx
+++ src/pages/Home/ContactSection/index.tsx
@@ -1,19 +1,89 @@
-import { Box, Typography, Card, CardContent, styled, useTheme, TextField, Grid, Button, Link } from "@mui/material";
+import { Box, Typography, Card, CardContent, styled, useTheme, TextField, Grid, Button, Link, Snackbar, SnackbarOrigin, Alert } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { BackgroundParticle } from "../../../lib/BackgroundParticle";
-import ambient from "../../../assets/ambient.svg"
 import { tokens } from "../../../styles/theme";
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import EmailIcon from '@mui/icons-material/Email';
 import { Icon } from "@iconify/react";
 import SendIcon from '@mui/icons-material/Send';
-import { BackgroundSky } from "../../../lib/BackgroundSky";
 import { Footbar } from "../../../components/Footbar";
+import { useState } from "react";
+import emailjs from '@emailjs/browser'
+import React from "react";
 
+interface State extends SnackbarOrigin {
+  open: boolean;
+}
 
 export function ContactSection() {
   const theme = useTheme()
   const colors = tokens(theme.palette.mode)
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [successSendEmail, setSuccessSendEmail] = useState(false);
+
+  const [checkForm, setCheckForm] = React.useState<State>({
+    open: false,
+    vertical: 'bottom',
+    horizontal: 'center',
+  });
+
+  const [openSnackbarSendEmail, setOpenSnackbarSendEmail] = React.useState<State>({
+    open: false,
+    vertical: 'bottom',
+    horizontal: 'center',
+  });
+
+  const [errorSendEmail, setErrorSendEmail] = React.useState<State>({
+    open: false,
+    vertical: 'bottom',
+    horizontal: 'center',
+  });
+
+  const handleName = (event: any) => {
+    setName(event.target.value);
+  };
+
+  const handleEmail = (event: any) => {
+    setEmail(event.target.value);
+  };
+  
+  const handleSendMessage = (event: any) => {
+    setMessage(event.target.value);
+  };
+
+  function HandleSendEmail() {
+    console.log('cheguei aqui')
+    if (name === '' || email === '' || message === '') {
+      setCheckForm({ ...checkForm, open: true });
+    } else {
+      const templateParams = {
+        from_name: name,
+        email: email,
+        message: message
+      }
+
+      emailjs.send('service_uts0hgf', 'template_lqtr0ae', templateParams, 'APpzSATRJEoaP-hPo')
+      .then((res) => {
+        setSuccessSendEmail(true)
+        setName('')
+        setEmail('')
+        setMessage('')
+
+        setOpenSnackbarSendEmail({ ...openSnackbarSendEmail, open: true });
+      }).catch((err) => {
+        setErrorSendEmail({ ...errorSendEmail, open: true });
+      });
+    }
+  }
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbarSendEmail({ ...openSnackbarSendEmail, open: false });
+    setCheckForm({ ...checkForm, open: false });
+    setErrorSendEmail({ ...errorSendEmail, open: false });
+  };
 
   return (
     <>
@@ -26,8 +96,8 @@ export function ContactSection() {
         justifyContent: "center",
         position: "relative"
       }}>
+        
         <BackgroundParticle height="100%" />
-        <BackgroundSky height="100%" />
 
         <Box sx={{
           width: "1120px",
@@ -37,6 +107,36 @@ export function ContactSection() {
           alignItems: "center",
           gap: 5,
         }}>
+          {checkForm && (
+            <>
+              <Snackbar open={checkForm.open} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+                <Alert onClose={handleCloseSnackbar} severity="warning" sx={{ width: '100%' }}>
+                  Informe todos os dados para envio do formulário!
+                </Alert>
+              </Snackbar>
+            </>
+          )}
+
+          {successSendEmail && (
+            <>
+              <Snackbar open={openSnackbarSendEmail.open} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+                <Alert onClose={handleCloseSnackbar} severity="success" sx={{ width: '100%' }}>
+                  Email enviado com sucesso!
+                </Alert>
+              </Snackbar>
+            </>
+          )}
+
+          {errorSendEmail && (
+            <>
+              <Snackbar open={errorSendEmail.open} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+                <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
+                  Ocorreu um erro ao enviar o email. Por favor, tente novamente mais tarde.
+                </Alert>
+              </Snackbar>
+            </>
+          )}
+
           <Typography variant="h2" color="primary">
             Entre em contato
           </Typography>
@@ -75,17 +175,19 @@ export function ContactSection() {
                 <CardContent>
                   <Box display="flex" flexDirection="column" gap={1}>
                     <Typography variant="h4" color="secondary">Send Me a Message</Typography>
-                    <TextField id="name" label="Your Name" variant="filled" sx={{ mt: "15px" }} />
-                    <TextField id="email" label="Your Email" variant="filled" />
+                    <TextField id="name" label="Your Name" variant="filled" sx={{ mt: "15px" }} onChange={handleName} value={name} />
+                    <TextField id="email" label="Your Email" variant="filled" onChange={handleEmail} value={email} />
                     <TextField
                       id="message"
-                      label="Menssage"
+                      label="Message"
                       variant="filled"
                       multiline
                       maxRows={4}
+                      value={message}
+                      onChange={handleSendMessage}
                     />
 
-                    <Button sx={{ mt: 3 }} startIcon={<SendIcon />} variant="contained">Send</Button>
+                    <Button sx={{ mt: 3 }} startIcon={<SendIcon />} variant="contained" onClick={() => HandleSendEmail()}>Send</Button>
                   </Box>
                 </CardContent>
               </Card>
